80c69f1f48523a7f7f61b241f8a25185baed2f2e
Ryan Vieira Souza <53231910+RyanV-Souza@users.noreply.github.com>
2020-04-15 09:21:02 -0300

    Inserção das Funções JS

---
+++ js/principal.js
@@ -0,0 +1,36 @@
+$(document).on("mouseover", ".sidebar", function(){
+    $(".fundo").show();
+
+    $(".fundo").css("background", "rgba(0,0,0,0.85)");
+    $(".fundo").css("position", "fixed");
+    $(".fundo").css("right", "0");
+    $(".fundo").css("left", "0");
+    $(".fundo").css("bottom", "0");
+    $(".fundo").css("top", "0");
+
+    $(".fundo").css("transition", "600ms");
+  });
+
+
+  $(document).on("mouseout", ".sidebar", function(){
+
+    $(".fundo").css("background", "rgba(0,0,0,0)");
+    $(".fundo").css("transition", "600ms");
+    $(".fundo").css("position", "");
+
+
+  }); 
+
+  
+
+  $(document).on("click", ".cadastrarUsuario", function(){
+      $("#modalCadastroUsuario").modal('show');
+  });
+
+  $(document).on("click", ".cadastrarComponente", function(){
+    $("#modalCadastroComponente").modal('show');
+});
+
+$(document).on("click", ".cadastrarLocal", function(){
+  $("#modalCadastroLocal").modal('show');
+});
