4461ba4048f86a1f2ca615557d744341a9456417
RyanV-Souza <ryanvsouza99@gmail.com>
2020-07-21 10:22:27 -0300

    Atualizando o SQL

--- sql/horus.sql
+++ sql/horus.sql
@@ -1,410 +1,172 @@
--- phpMyAdmin SQL Dump
--- version 4.7.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: 22-Abr-2020 às 15:08
--- Versão do servidor: 5.6.34
--- PHP Version: 7.1.11
+CREATE DATABASE IF NOT EXISTS horus DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE horus;
+CREATE TABLE IF NOT EXISTS tb_aluno (
+  cd_rmAluno int(11) NOT NULL,
+  nm_aluno varchar(150) NOT NULL,
+  nr_cpf char(11) NOT NULL,
+  ds_statusALuno varchar(45) NOT NULL,
+  FK_cd_grupoSala int(11) DEFAULT NULL,
+  FK_cd_modulo int(11) NOT NULL,
+  PRIMARY KEY (cd_rmAluno),
+  KEY FK_cd_modulo (FK_cd_modulo),
+  KEY FK_cd_grupoSala (FK_cd_grupoSala)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS tb_campoestagio (
+  cd_campoEstagio int(11) NOT NULL AUTO_INCREMENT,
+  nm_campoEstagio varchar(100) NOT NULL,
+  sg_campoEstagio varchar(45) DEFAULT NULL,
+  ds_enderecoEstagio char(8) DEFAULT NULL,
+  ds_statusCampoEstagio varchar(45) NOT NULL,
+  PRIMARY KEY (cd_campoEstagio)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE IF NOT EXISTS tb_componentecampoestagio (
+  cd_componentecampoestagio int(11) NOT NULL AUTO_INCREMENT,
+  FK_cd_componente int(11) DEFAULT NULL,
+  FK_cd_campoestagio int(11) DEFAULT NULL,
+  PRIMARY KEY (cd_componentecampoestagio),
+  KEY FK_cd_componente (FK_cd_componente),
+  KEY FK_cd_campoestagio (FK_cd_campoestagio)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Database: `horus`
---
-  create database horus;
-  use horus;
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS tb_componentecampoestagioprofessor (
+  cd_componenteCampoEstagioProfessor int(11) NOT NULL AUTO_INCREMENT,
+  FK_cd_componentecampoestagio int(11) DEFAULT NULL,
+  FK_cd_professor int(11) DEFAULT NULL,
+  PRIMARY KEY (cd_componenteCampoEstagioProfessor),
+  KEY FK_cd_componentecampoestagio (FK_cd_componentecampoestagio),
+  KEY FK_cd_professor (FK_cd_professor)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Estrutura da tabela `tb_aluno`
---
+CREATE TABLE IF NOT EXISTS tb_componentecampoestagioprofessor_modulo (
+  cd_componenteCampoEstagioProfessor_Modulo int(11) NOT NULL AUTO_INCREMENT,
+  FK_cd_componenteCampoEstagioProfessor int(11) NOT NULL,
+  FK_cd_modulo int(11) NOT NULL,
+  PRIMARY KEY (cd_componenteCampoEstagioProfessor_Modulo),
+  KEY FK_cd_componenteCampoEstagioProfessor (FK_cd_componenteCampoEstagioProfessor),
+  KEY FK_cd_modulo (FK_cd_modulo)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS tb_componentecurricular (
+  cd_componente int(11) NOT NULL AUTO_INCREMENT,
+  nm_componente varchar(100) NOT NULL,
+  ds_duracaoComponente int(11) NOT NULL,
+  ds_statusComponente varchar(45) NOT NULL,
+  ds_restricaoModulo char(1) NOT NULL,
+  PRIMARY KEY (cd_componente)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tb_aluno` (
-  `cd_rmAluno` int(11) NOT NULL,
-  `nm_aluno` varchar(150) NOT NULL,
-  `nr_cpf` char(11) NOT NULL,
-  `ds_statusALuno` varchar(45) NOT NULL,
-  `FK_cd_grupoSala` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS tb_cronograma (
+  cd_cronograma int(11) NOT NULL AUTO_INCREMENT,
+  ds_diretorioArquivo varchar(200) DEFAULT NULL,
+  PRIMARY KEY (cd_cronograma)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS tb_datavalidacronograma (
+  cd_dataValida int(11) NOT NULL AUTO_INCREMENT,
+  dt_inicioData datetime NOT NULL,
+  dt_fimData datetime NOT NULL,
+  ds_corBloco varchar(20) NOT NULL,
+  FK_cd_componentecampoestagioprofessor_modulo int(11) NOT NULL,
+  FK_cd_cronograma int(11) NOT NULL,
+  FK_cd_grupoSala int(11) NOT NULL,
+  PRIMARY KEY (cd_dataValida),
+  KEY FK_cd_componentecampoestagioprofessor_modulo (FK_cd_componentecampoestagioprofessor_modulo),
+  KEY FK_cd_cronograma (FK_cd_cronograma),
+  KEY FK_cd_grupoSala (FK_cd_grupoSala)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
---
--- Estrutura da tabela `tb_campoestagio`
---
+CREATE TABLE IF NOT EXISTS tb_excessaocronograma (
+  cd_excessao int(11) NOT NULL AUTO_INCREMENT,
+  dt_diaExcessao datetime NOT NULL,
+  dt_diaExcessaoFim datetime NOT NULL,
+  FK_cd_cronograma int(11) NOT NULL,
+  PRIMARY KEY (cd_excessao),
+  KEY FK_cd_cronograma (FK_cd_cronograma)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tb_campoestagio` (
-  `cd_campoEstagio` int(11) NOT NULL,
-  `nm_campoEstagio` varchar(100) NOT NULL,
-  `sg_campoEstagio` varchar(45) DEFAULT NULL,
-  `ds_enderecoEstagio` char(8) DEFAULT NULL,
-  `ds_statusCampoEstagio` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS tb_gruposala (
+  cd_grupoSala int(11) NOT NULL AUTO_INCREMENT,
+  sg_grupoSala varchar(45) NOT NULL,
+  FK_cd_modulo int(11) NOT NULL,
+  nm_grupoSala varchar(50) NOT NULL,
+  ds_statusGrupo varchar(50) NOT NULL,
+  PRIMARY KEY (cd_grupoSala),
+  KEY FK_cd_modulo (FK_cd_modulo)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS tb_modulo (
+  cd_modulo int(11) NOT NULL AUTO_INCREMENT,
+  sg_modulo varchar(45) NOT NULL,
+  FK_cd_cronograma int(11) DEFAULT NULL,
+  ds_statusModulo varchar(50) NOT NULL,
+  ds_numeroModulo char(1) NOT NULL,
+  dt_ano varchar(4) NOT NULL,
+  ds_semestre char(1) NOT NULL,
+  PRIMARY KEY (cd_modulo),
+  KEY FK_cd_cronograma (FK_cd_cronograma)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Estrutura da tabela `tb_componentecampoestagiocronograma`
---
+CREATE TABLE IF NOT EXISTS tb_nivelacesso (
+  cd_acesso int(11) NOT NULL AUTO_INCREMENT,
+  nm_nivel varchar(100) NOT NULL,
+  PRIMARY KEY (cd_acesso)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tb_componentecampoestagiocronograma` (
-  `cd_componenteCampoEstagioCronograma` int(11) NOT NULL,
-  `FK_cd_componente` int(11) NOT NULL,
-  `FK_cd_campoEstagio` int(11) NOT NULL,
-  `FK_cd_cronograma` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_componentecurricular`
---
-
-CREATE TABLE `tb_componentecurricular` (
-  `cd_componente` int(11) NOT NULL,
-  `nm_componente` varchar(100) NOT NULL,
-  `ds_componente` longtext NOT NULL,
-  `ds_duracaoComponente` int(11) NOT NULL,
-  `ds_statusComponente` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_cronograma`
---
-
-CREATE TABLE `tb_cronograma` (
-  `cd_cronograma` int(11) NOT NULL,
-  `ds_diretorioArquivo` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_excessaocronograma`
---
-
-CREATE TABLE `tb_excessaocronograma` (
-  `cd_excessao` int(11) NOT NULL,
-  `dt_diaExcessao` date NOT NULL,
-  `ds_motivoExcessao` varchar(200) NOT NULL,
-  `ds_statusExecessao` varchar(45) NOT NULL,
-  `FK_cd_cronograma` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_gruposala`
---
-
-CREATE TABLE `tb_gruposala` (
-  `cd_grupoSala` int(11) NOT NULL,
-  `sg_grupoSala` varchar(45) NOT NULL,
-  `FK_cd_modulo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_modulo`
---
-
-CREATE TABLE `tb_modulo` (
-  `cd_modulo` int(11) NOT NULL,
-  `dt_inicial` date NOT NULL,
-  `dt_final` date NOT NULL,
-  `sg_modulo` varchar(45) NOT NULL,
-  `FK_cd_cronograma` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_nivelacesso`
---
-
-CREATE TABLE `tb_nivelacesso` (
-  `cd_acesso` int(11) NOT NULL,
-  `nm_nivel` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `tb_nivelacesso`
---
-
-INSERT INTO `tb_nivelacesso` (`cd_acesso`, `nm_nivel`) VALUES
+ /Esse INSERT é essencial para o Banco de Dados, não excluir/
+INSERT INTO tb_nivelacesso (cd_acesso, nm_nivel) VALUES
 (1, 'Coordenador'),
 (2, 'Professor');
+/Esse INSERT é essencial para o Banco de Dados, não excluir/
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS tb_usuario (
+  cd_rmUsuario int(11) NOT NULL,
+  nm_usuario varchar(200) NOT NULL,
+  nr_cpf char(11) NOT NULL,
+  ds_statusUsuario varchar(45) NOT NULL,
+  FK_cd_acesso int(11) NOT NULL,
+  ds_emailUsuario varchar(250) NOT NULL,
+  ds_senha varchar(100) NOT NULL,
+  PRIMARY KEY (cd_rmUsuario),
+  KEY FK_cd_acesso (FK_cd_acesso)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Estrutura da tabela `tb_relatorio`
---
+/Esse INSERT é essencial para o Banco de Dados, não excluir/
+INSERT INTO tb_usuario (cd_rmUsuario, nm_usuario, nr_cpf, ds_statusUsuario, FK_cd_acesso, ds_emailUsuario, ds_senha) VALUES
+(1111, 'Administrador', '45124715022', 'Ativado', 1, 'admin@admin', MD5('etec123'));
+/Esse INSERT é essencial para o Banco de Dados, não excluir/
 
-CREATE TABLE `tb_relatorio` (
-  `cd_relatorio` int(11) NOT NULL,
-  `ds_relatorio` longtext NOT NULL,
-  `dt_relatorio` char(11) NOT NULL,
-  `FK_cd_rmUsuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE tb_aluno
+  ADD CONSTRAINT tb_aluno_ibfk_1 FOREIGN KEY (FK_cd_grupoSala) REFERENCES tb_gruposala (cd_grupoSala),
+  ADD CONSTRAINT tb_aluno_ibfk_2 FOREIGN KEY (FK_cd_modulo) REFERENCES tb_modulo (cd_modulo);
 
--- --------------------------------------------------------
+ALTER TABLE tb_componentecampoestagio
+  ADD CONSTRAINT tb_componentecampoestagio_ibfk_1 FOREIGN KEY (FK_cd_componente) REFERENCES tb_componentecurricular (cd_componente),
+  ADD CONSTRAINT tb_componentecampoestagio_ibfk_2 FOREIGN KEY (FK_cd_campoestagio) REFERENCES tb_campoestagio (cd_campoEstagio);
 
---
--- Estrutura da tabela `tb_usuario`
---
+ALTER TABLE tb_componentecampoestagioprofessor
+  ADD CONSTRAINT tb_componentecampoestagioprofessor_ibfk_1 FOREIGN KEY (FK_cd_componentecampoestagio) REFERENCES tb_componentecampoestagio (cd_componentecampoestagio),
+  ADD CONSTRAINT tb_componentecampoestagioprofessor_ibfk_2 FOREIGN KEY (FK_cd_professor) REFERENCES tb_usuario (cd_rmUsuario);
 
-CREATE TABLE `tb_usuario` (
-  `cd_rmUsuario` int(11) NOT NULL,
-  `nm_usuario` varchar(200) NOT NULL,
-  `nr_cpf` char(11) NOT NULL,
-  `dt_nascimento` date NOT NULL,
-  `ds_statusUsuario` varchar(45) NOT NULL,
-  `FK_cd_acesso` int(11) NOT NULL,
-  `ds_emailUsuario` varchar(250) NOT NULL,
-  `nr_telefoneUsuario` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `tb_usuario`
---
+ALTER TABLE tb_componentecampoestagioprofessor_modulo
+  ADD CONSTRAINT tb_componentecampoestagioprofessor_modulo_ibfk_1 FOREIGN KEY (FK_cd_componenteCampoEstagioProfessor) REFERENCES tb_componentecampoestagioprofessor (cd_componenteCampoEstagioProfessor),
+  ADD CONSTRAINT tb_componentecampoestagioprofessor_modulo_ibfk_2 FOREIGN KEY (FK_cd_modulo) REFERENCES tb_modulo (cd_modulo);
 
-INSERT INTO `tb_usuario` (`cd_rmUsuario`, `nm_usuario`, `nr_cpf`, `dt_nascimento`, `ds_statusUsuario`, `FK_cd_acesso`, `ds_emailUsuario`, `nr_telefoneUsuario`) VALUES
-(12, 'Ryan', '111', '0000-00-00', 'Ativado', 1, 'teste@teste', '111'),
-(1111, 'Ryan', '111', '0000-00-00', 'Ativado', 1, 'teste@teste', '111');
+ALTER TABLE tb_datavalidacronograma
+  ADD CONSTRAINT tb_datavalidacronograma_ibfk_1 FOREIGN KEY (FK_cd_componentecampoestagioprofessor_modulo) REFERENCES tb_componentecampoestagioprofessor_modulo (cd_componenteCampoEstagioProfessor_Modulo),
+  ADD CONSTRAINT tb_datavalidacronograma_ibfk_2 FOREIGN KEY (FK_cd_cronograma) REFERENCES tb_cronograma (cd_cronograma),
+  ADD CONSTRAINT tb_datavalidacronograma_ibfk_3 FOREIGN KEY (FK_cd_grupoSala) REFERENCES tb_gruposala (cd_grupoSala);
 
--- --------------------------------------------------------
+ALTER TABLE tb_excessaocronograma
+  ADD CONSTRAINT tb_excessaocronograma_ibfk_1 FOREIGN KEY (FK_cd_cronograma) REFERENCES tb_cronograma (cd_cronograma);
 
---
--- Estrutura da tabela `tb_usuariocronograma`
---
+ALTER TABLE tb_gruposala
+  ADD CONSTRAINT tb_gruposala_ibfk_1 FOREIGN KEY (FK_cd_modulo) REFERENCES tb_modulo (cd_modulo);
 
-CREATE TABLE `tb_usuariocronograma` (
-  `cd_usuarioCronograma` int(11) NOT NULL,
-  `FK_cd_rmUsuario` int(11) NOT NULL,
-  `FK_cd_cronograma` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_aluno`
---
-ALTER TABLE `tb_aluno`
-  ADD PRIMARY KEY (`cd_rmAluno`),
-  ADD KEY `FK_cd_grupoSala` (`FK_cd_grupoSala`);
-
---
--- Indexes for table `tb_campoestagio`
---
-ALTER TABLE `tb_campoestagio`
-  ADD PRIMARY KEY (`cd_campoEstagio`);
-
---
--- Indexes for table `tb_componentecampoestagiocronograma`
---
-ALTER TABLE `tb_componentecampoestagiocronograma`
-  ADD PRIMARY KEY (`cd_componenteCampoEstagioCronograma`),
-  ADD KEY `FK_cd_componente` (`FK_cd_componente`),
-  ADD KEY `FK_cd_campoEstagio` (`FK_cd_campoEstagio`),
-  ADD KEY `FK_cd_cronograma` (`FK_cd_cronograma`);
-
---
--- Indexes for table `tb_componentecurricular`
---
-ALTER TABLE `tb_componentecurricular`
-  ADD PRIMARY KEY (`cd_componente`);
-
---
--- Indexes for table `tb_cronograma`
---
-ALTER TABLE `tb_cronograma`
-  ADD PRIMARY KEY (`cd_cronograma`);
-
---
--- Indexes for table `tb_excessaocronograma`
---
-ALTER TABLE `tb_excessaocronograma`
-  ADD PRIMARY KEY (`cd_excessao`),
-  ADD KEY `FK_cd_cronograma` (`FK_cd_cronograma`);
-
---
--- Indexes for table `tb_gruposala`
---
-ALTER TABLE `tb_gruposala`
-  ADD PRIMARY KEY (`cd_grupoSala`),
-  ADD KEY `FK_cd_modulo` (`FK_cd_modulo`);
-
---
--- Indexes for table `tb_modulo`
---
-ALTER TABLE `tb_modulo`
-  ADD PRIMARY KEY (`cd_modulo`),
-  ADD KEY `FK_cd_cronograma` (`FK_cd_cronograma`);
-
---
--- Indexes for table `tb_nivelacesso`
---
-ALTER TABLE `tb_nivelacesso`
-  ADD PRIMARY KEY (`cd_acesso`);
-
---
--- Indexes for table `tb_relatorio`
---
-ALTER TABLE `tb_relatorio`
-  ADD PRIMARY KEY (`cd_relatorio`),
-  ADD KEY `FK_cd_rmUsuario` (`FK_cd_rmUsuario`);
-
---
--- Indexes for table `tb_usuario`
---
-ALTER TABLE `tb_usuario`
-  ADD PRIMARY KEY (`cd_rmUsuario`),
-  ADD KEY `FK_cd_acesso` (`FK_cd_acesso`);
-
---
--- Indexes for table `tb_usuariocronograma`
---
-ALTER TABLE `tb_usuariocronograma`
-  ADD PRIMARY KEY (`cd_usuarioCronograma`),
-  ADD KEY `FK_cd_rmUsuario` (`FK_cd_rmUsuario`),
-  ADD KEY `FK_cd_cronograma` (`FK_cd_cronograma`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tb_campoestagio`
---
-ALTER TABLE `tb_campoestagio`
-  MODIFY `cd_campoEstagio` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_componentecampoestagiocronograma`
---
-ALTER TABLE `tb_componentecampoestagiocronograma`
-  MODIFY `cd_componenteCampoEstagioCronograma` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_componentecurricular`
---
-ALTER TABLE `tb_componentecurricular`
-  MODIFY `cd_componente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_cronograma`
---
-ALTER TABLE `tb_cronograma`
-  MODIFY `cd_cronograma` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_excessaocronograma`
---
-ALTER TABLE `tb_excessaocronograma`
-  MODIFY `cd_excessao` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_gruposala`
---
-ALTER TABLE `tb_gruposala`
-  MODIFY `cd_grupoSala` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_modulo`
---
-ALTER TABLE `tb_modulo`
-  MODIFY `cd_modulo` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_nivelacesso`
---
-ALTER TABLE `tb_nivelacesso`
-  MODIFY `cd_acesso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tb_relatorio`
---
-ALTER TABLE `tb_relatorio`
-  MODIFY `cd_relatorio` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tb_usuariocronograma`
---
-ALTER TABLE `tb_usuariocronograma`
-  MODIFY `cd_usuarioCronograma` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `tb_aluno`
---
-ALTER TABLE `tb_aluno`
-  ADD CONSTRAINT `tb_aluno_ibfk_1` FOREIGN KEY (`FK_cd_grupoSala`) REFERENCES `tb_gruposala` (`cd_grupoSala`);
-
---
--- Limitadores para a tabela `tb_componentecampoestagiocronograma`
---
-ALTER TABLE `tb_componentecampoestagiocronograma`
-  ADD CONSTRAINT `tb_componentecampoestagiocronograma_ibfk_1` FOREIGN KEY (`FK_cd_componente`) REFERENCES `tb_componentecurricular` (`cd_componente`),
-  ADD CONSTRAINT `tb_componentecampoestagiocronograma_ibfk_2` FOREIGN KEY (`FK_cd_campoEstagio`) REFERENCES `tb_campoestagio` (`cd_campoEstagio`),
-  ADD CONSTRAINT `tb_componentecampoestagiocronograma_ibfk_3` FOREIGN KEY (`FK_cd_cronograma`) REFERENCES `tb_cronograma` (`cd_cronograma`);
-
---
--- Limitadores para a tabela `tb_excessaocronograma`
---
-ALTER TABLE `tb_excessaocronograma`
-  ADD CONSTRAINT `tb_excessaocronograma_ibfk_1` FOREIGN KEY (`FK_cd_cronograma`) REFERENCES `tb_cronograma` (`cd_cronograma`);
-
---
--- Limitadores para a tabela `tb_gruposala`
---
-ALTER TABLE `tb_gruposala`
-  ADD CONSTRAINT `tb_gruposala_ibfk_1` FOREIGN KEY (`FK_cd_modulo`) REFERENCES `tb_modulo` (`cd_modulo`);
-
---
--- Limitadores para a tabela `tb_modulo`
---
-ALTER TABLE `tb_modulo`
-  ADD CONSTRAINT `tb_modulo_ibfk_1` FOREIGN KEY (`FK_cd_cronograma`) REFERENCES `tb_cronograma` (`cd_cronograma`);
-
---
--- Limitadores para a tabela `tb_relatorio`
---
-ALTER TABLE `tb_relatorio`
-  ADD CONSTRAINT `tb_relatorio_ibfk_1` FOREIGN KEY (`FK_cd_rmUsuario`) REFERENCES `tb_usuario` (`cd_rmUsuario`);
-
---
--- Limitadores para a tabela `tb_usuario`
---
-ALTER TABLE `tb_usuario`
-  ADD CONSTRAINT `tb_usuario_ibfk_1` FOREIGN KEY (`FK_cd_acesso`) REFERENCES `tb_nivelacesso` (`cd_acesso`);
-
---
--- Limitadores para a tabela `tb_usuariocronograma`
---
-ALTER TABLE `tb_usuariocronograma`
-  ADD CONSTRAINT `tb_usuariocronograma_ibfk_1` FOREIGN KEY (`FK_cd_rmUsuario`) REFERENCES `tb_usuario` (`cd_rmUsuario`),
-  ADD CONSTRAINT `tb_usuariocronograma_ibfk_2` FOREIGN KEY (`FK_cd_cronograma`) REFERENCES `tb_cronograma` (`cd_cronograma`);
+ALTER TABLE tb_modulo
+  ADD CONSTRAINT tb_modulo_ibfk_1 FOREIGN KEY (FK_cd_cronograma) REFERENCES tb_cronograma (cd_cronograma);
+ALTER TABLE tb_usuario
+  ADD CONSTRAINT tb_usuario_ibfk_1 FOREIGN KEY (FK_cd_acesso) REFERENCES tb_nivelacesso (cd_acesso);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
